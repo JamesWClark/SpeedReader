7ef806a63e1e93c5f5b577878cde23fd1faf9b3d
JamesWClark <jwclark@rockhursths.edu>
2016-04-20 23:57:08 -0500

    speed reader alpha

--- main.js
+++ main.js
@@ -1,36 +1,84 @@
 $(document).ready(function() {
 
     var speed = 301;
+    var pause = false;
+    var words = [];
+    var i = 0; // current word index
     
     var setup = function(data) {
-        var words = data.split(' ');
-        nextWord(words, 0);
+        words = data.split(' ');
+        console.log('setting up: there are ' + words.length + ' words');
+        draw();
     };
     
-    var nextWord = function(words, i) {
-        $('[id^="c"]').text('');
-        var wordLength = words[i].length;
-        var middle = parseInt(wordLength / 2) - 1;
-        var ui = 13;
-        $('#c' + ui).text(words[i][middle]);
-        for(var j = middle - 1; j >= 0; j--) {
-            $('#c' + --ui).text(words[i][j]);
+    var draw = function() {
+        console.log('drawing');
+        if(!pause) {
+            displayWord(words[i++]);
         }
-        ui = 13
-        for(var k = middle + 1; k < wordLength; k++) {
-            $('#c' + ++ui).text(words[i][k]);
-        }
-        
-        
-        console.log(words[i++]);
-        if(i < words.length) {
-            setTimeout(function() {
-                nextWord(words, i);
-            }, speed);
+        setTimeout(draw, speed);
+    };
+    
+    var displayWord = function(word) {
+        if(i > -1 && i < words.length) {
+            // first, clear the last word, or initialize the first one :)
+            $('[id^="c"]').text('');
+
+            // second, calculate some positional data
+            var wordLength = word.length;
+            var middle = parseInt(wordLength / 2);
+            var ui = 13; // the middle ui element in print
+
+            // print the middle element
+            $('#c' + ui).text(word[middle]);
+
+            // from middle to beginning, print
+            for(var j = middle - 1; j >= 0; j--) {
+                $('#c' + --ui).text(word[j]);
+            }
+
+            // from middle to end, print
+            ui = 13
+            for(var k = middle + 1; k < wordLength; k++) {
+                $('#c' + ++ui).text(word[k]);
+            }
+        } else {
+            if(i < 0) {
+                i = 0;
+            } else if (i >= words.length) {
+                i = words.length - 1;
+            }
+            displayWord(words[i]);
         }
     };
     
+    $(document).on('keypress', function(e) {
+        var key = String.fromCharCode(e.which);
+        switch(key.toLowerCase()) {
+            case 'p':
+                if(pause) {
+                    pause = false;
+                } else {
+                    pause = true;
+                }
+                break;
+        }
+    });
     
+    $(document).on('keydown', function(e) {
+        switch(e.keyCode) {
+            case 37: // left arrow
+                pause = true;
+                i--;
+                displayWord(words[i]);
+                break;
+            case 39: // right arrow
+                pause = true;
+                i++;
+                displayWord(words[i]);
+                break;
+        }
+    });
     
     $.get('CS1.txt', function(data) {
         setup(data);
